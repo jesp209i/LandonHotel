0e5388dd790496dee8252e0d5d27edba4e180b98
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-09-28 18:47:47 +0200

    Add project files.

---
+++ LandonHotel.Tests/BookingControllerTest.cs
@@ -0,0 +1,20 @@
+using LandonHotel.Services;
+using System;
+using Xunit;
+
+namespace LandonHotel.Tests
+{
+    public class BookingControllerTest
+    {
+        IRoomService roomService;
+        public BookingControllerTest()
+        {
+
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
